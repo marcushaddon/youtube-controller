77a30e61fd7b2a167e70ce53b05e636e287f95fa
Marcus Haddon <marcus.haddon@nike.com>
2019-05-04 17:30:15 -0700

    implement queueTrack method

--- demo/app.ts
+++ demo/app.ts
@@ -1,10 +1,24 @@
 import PlayerController from '../src/PlayerController';
+import Track from '../src/Track';
 declare global {
     interface Window { onYouTubeIframeAPIReady: any; }
 }
 
+const tracks: Track[] = [
+    {
+        title: 'Thing',
+        videoId: 'l0vrsO3_HpU',
+        start: 124,
+        stop: Infinity,
+    }
+];
+
 window.onYouTubeIframeAPIReady = function() {
     const pc = new PlayerController();
-    console.log(pc)
+    pc.initted
+    .subscribe(initted => {
+        if (initted) {
+            pc.loadTrackList(tracks);
+        }
+    });
 }
-
--- src/PlayerController.ts
+++ src/PlayerController.ts
@@ -1,12 +1,14 @@
+// TODO: Find better solution for this maybe (regeneratorruntime is not defined)?
+import 'babel-polyfill';
 import { BehaviorSubject } from 'rxjs';
 import Track from './Track';
 
 export enum PlayerState {
+    Idle,
     Playing,
     Paused,
     Stopped,
     Loading,
-    Idle,
     Error,
 }
 
@@ -18,11 +20,16 @@ export default class PlayerController {
     // Abstractions
     private _activeTrack?: Track;
     private _tracks: Track[];
-    public readonly activeTrackIndex: number;
+    private _activeTrackIndex;
     private _playbackPosition: number;
     private _state: PlayerState;
 
+
+    public initted: BehaviorSubject<boolean>;
+    public isInitted = false;
+
     public readonly trackChanged: BehaviorSubject<Track | undefined>;
+    public readonly activeTrackIndexChanged: BehaviorSubject<number>;
     public readonly tracksChanged: BehaviorSubject<Track[]>;
     // NOTE: Relative to start of track
     public readonly progress: BehaviorSubject<number>;
@@ -31,21 +38,24 @@ export default class PlayerController {
     public constructor() {
         // TODO: Create new player
         this._player = new YT.Player('player', {
-            height: '390',
-            width: '640',
-            videoId: 'M7lc1UVf-VE',
+            height: '1000',
+            width: '1000',
+            videoId: '',
             events: {
-              'onReady': this._onPlayerReady,
-              'onStateChange': this._onPlayerStateChange
+            //   'onReady': this._onPlayerReady,
+                'onReady': this._onPlayerReady.bind(this),
+                'onStateChange': this._onPlayerStateChange.bind(this)
             }
-          });
-        this._playerStateChanged = new BehaviorSubject<YT.PlayerState>(-1);
+        });
+        this.initted = new BehaviorSubject(false);
+        this._playerStateChanged = new BehaviorSubject<YT.PlayerState>(YT.PlayerState.UNSTARTED);
 
         this._tracks = [];
-        this.activeTrackIndex = -1;
+        this._activeTrackIndex = -1;
         this._playbackPosition = -1;
         this._state = PlayerState.Idle;
         this.trackChanged = new BehaviorSubject(this._activeTrack);
+        this.activeTrackIndexChanged = new BehaviorSubject(-1);
         this.tracksChanged = new BehaviorSubject(this._tracks);
         this.progress = new BehaviorSubject(0);
         this.stateChanged = new BehaviorSubject<PlayerState>(this._state);
@@ -53,11 +63,80 @@ export default class PlayerController {
 
     private _onPlayerReady = (event: YT.PlayerEvent) => {
         this._player = event.target;
+        this.isInitted = true;
+        console.log("I am initted")
+        this.initted.next(true);
     }
 
     // For using in the construction of the iFrame player
-    private _onPlayerStateChange(event: YT.OnStateChangeEvent): void {
+    public _onPlayerStateChange(event: YT.OnStateChangeEvent): void {
         this._playerStateChanged.next(event.data);
     }
+
+    /**
+     * Make player operations awaitable
+     */
+    private _loadVideo(id: string, start?: number): Promise<YT.PlayerError> {
+        return new Promise((res, rej) => {
+            // Wait till initted
+            this._player.loadVideoById(id, start);
+            this._playerStateChanged
+            .subscribe(state => {
+                if (state === YT.PlayerState.PLAYING) {
+                    // TODO: Determine if we should let it play?
+                    res();
+                }
+            })
+        });
+    }
+
+    /**
+     * Should not be called while dragging!
+     */
+    private _seekTo(time: number): Promise<void> {
+        return new Promise((res, rej) => {
+            this._player.seekTo(time, true);
+            this._playerStateChanged
+            .subscribe(state => {
+                // TODO: Determine what state entails 'done'
+                if (state === YT.PlayerState.PLAYING ||
+                    state === YT.PlayerState.PAUSED) {
+                    res();
+                }
+            });
+        });
+    }
+
+    /**
+     * Set a single track as active.
+     */
+    public async queueTrack(track: Track): Promise<void> {
+        const prevVideoId = this._activeTrack && this._activeTrack.videoId;
+        this._activeTrack = track;
+        if (this._activeTrack.videoId !== prevVideoId) {
+            await this._loadVideo(track.videoId, track.start);
+        } else {
+            await this._seekTo(track.start);
+        }
+    }
+
+    /**
+     * Load playlist
+     */
+    public loadTrackList(tracks: Track[]): void {
+        this._tracks = tracks;
+        this.queueTrack(tracks[0]);
+        this._activeTrackIndex = 0;
+        this.activeTrackIndexChanged.next(0);
+        this.trackChanged.next(this._activeTrack);
+    }
+
+    // /**
+    //  * Play
+    //  */
+    // public play(): Promise<void> {
+    //     // TODO: Handle no track, end of track etc
+        
+    // }
 
 }
